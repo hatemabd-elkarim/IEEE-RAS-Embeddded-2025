b120bc901d0c225320a8c26161f43dd52638ea07
Hatem Ayman <hatem.ayman.508@gmail.com>
2025-03-27 00:37:29 +0200

    Problem 8 Found an alternative for using global variable

---
+++ Task_2/Problem_8.C
@@ -0,0 +1,19 @@
+/*
+    3n+1 sequence:
+    https://codeforces.com/group/MWSDmqGsZm/contest/223339/submission/312628315
+*/
+#include <stdio.h>
+int sequence(int N);
+int main(){
+    int N;
+    scanf("%d", &N);
+    printf("%d", sequence(N));
+}
+int sequence(int N){
+    if (N == 1)
+        return 1;
+    if (N % 2)
+         return 1 + sequence (3 * N + 1);
+    else 
+         return 1 + sequence (N / 2);
+}
