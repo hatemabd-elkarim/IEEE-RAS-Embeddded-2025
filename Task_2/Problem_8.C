59c959578f26b8e05ee26cc18dfaef0c277bf9e4
Hatem Ayman <hatem.ayman.508@gmail.com>
2025-03-27 00:29:28 +0200

    Problem 8

--- Task_2/Problem_8.C
+++ Task_2/Problem_8.C
@@ -1,23 +1,19 @@
 /*
     3n+1 sequence:
-    https://codeforces.com/group/MWSDmqGsZm/contest/223339/submission/312628025
+    https://codeforces.com/group/MWSDmqGsZm/contest/223339/submission/312628315
 */
 #include <stdio.h>
-int count = 1;
-void sequence(int N);
+int sequence(int N);
 int main(){
     int N;
     scanf("%d", &N);
-    sequence(N);
-    printf("%d", count);
+    printf("%d", sequence(N));
 }
-void sequence(int N){
+int sequence(int N){
     if (N == 1)
-        return;
-    count++;
+        return 1;
     if (N % 2)
-        return sequence (3 * N + 1);
+         return 1 + sequence (3 * N + 1);
     else 
-        return sequence (N / 2);
-    
+         return 1 + sequence (N / 2);
 }
