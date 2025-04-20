3ee66d27df0b53f39cad174408f9041f70040171
Hatem Ayman <hatem.ayman.508@gmail.com>
2025-04-21 00:35:30 +0200

    Problem 4

---
+++ Task_4/Problem_4.C
@@ -0,0 +1,23 @@
+/*
+-Write a function that prints even number from a string, starting with the first character, followed by a new line.
+“Str = ‘012345678’”.
+
+Output:
+02468
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+void print_even(char *str);
+int main(){
+    char str[100];
+    scanf("%s", str);
+    print_even(str);
+    return 0;
+}
+void print_even(char *str){
+    for (int i = 0; str[i] != '\0'; i++ ){
+        if (isdigit(str[i]) && (str[i] - '0' ) % 2 == 0 ) printf("%c", str[i]);
+    }
+}
